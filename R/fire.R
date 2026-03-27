26343c50b3b66620facc9c9d4eb0cf2aa9eb7a3e
SladeSmelser <slade.smelser@gmail.com>
2026-03-27 11:52:38 -0500

    Adding data to package

---
+++ R/fire.R
@@ -0,0 +1,14 @@
+#' Fire Damage Data
+#'
+#' A dataset containing measurements of distance from the fire station and
+#' resulting fire damage costs. These data come from the FIREDAM.csv file used
+#' in MATH 4753 Laboratory 7.
+#'
+#' @format A data frame with 15 rows and 2 columns:
+#' \describe{
+#'   \item{DISTANCE}{Distance from the fire station (in unspecified units).}
+#'   \item{DAMAGE}{Amount of fire damage (in unspecified units).}
+#' }
+#'
+#' @source Original FIREDAM.csv file provided in MATH 4753 Lab 7.
+"fire"
